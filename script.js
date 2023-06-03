efced1a67e1868b30dc0c2289b462ec53efc0852
dobahsi <hsi.en07@nycu.edu.tw>
2023-06-03 18:25:03 +0800

    accessibility functions created

--- script.js
+++ script.js
@@ -20,11 +20,110 @@ function toggleMenu() {
 }
 toggleMenu()
 
-const popup = document.getElementById('accessibility-popup');
 
-function toggleAccessPopup() {
-    popup.classList.toggle('accessibility-hidden');
+var translatePercentage = 20;
+
+function goToLayer(layer) {
+    var mainWindow = document.querySelector('.nav-layer-1');
+    var layerWindow = document.querySelector(`.nav-layer-${layer}`);
+
+    layerWindow.classList.remove('displaynone');
+
+    setTimeout(() => {
+        mainWindow.style.transform = `translateX(-${translatePercentage}%)`;
+        mainWindow.style.opacity = '0';
+    }, 10);
+    
+    setTimeout(() => {
+        layerWindow.style.transform = 'translateX(0)';
+        layerWindow.style.opacity = '1';
+        mainWindow.classList.add('displaynone');
+    }, 100);
 }
+goToLayer(2)
+
+function backToFirstLayer(layer) {
+    var mainWindow = document.querySelector('.nav-layer-1');
+    var layerWindow = document.querySelector(`.nav-layer-${layer}`);
+
+    mainWindow.classList.remove('displaynone');
+
+    setTimeout(() => {
+        layerWindow.style.transform = `translateX(${translatePercentage}%)`;
+        layerWindow.style.opacity = '0';
+    }, 10);
+    
+    setTimeout(() => {
+        mainWindow.style.transform = 'translateX(0)';
+        mainWindow.style.opacity = '1';
+        layerWindow.classList.add('displaynone');
+    }, 100);
+}
+
+// accessibility settings
+// var rootElement = document.documentElement;
+var allElements = document.querySelector('*');
+
+var fontSizeStep = 0, letterSpacingStep = 0;
+
+function changeFontSize() {
+    fontSizeStep++;
+    var step = fontSizeStep % 3;
+    if (step === 0) {
+        allElements.style.fontSize = '100%';
+    } else if (step === 1) {
+        allElements.style.fontSize = '125%';
+    } else if (step === 2) {
+        allElements.style.fontSize = '87.5%';
+    }
+    var btnInnerText = document.querySelector(".font-size-btn").querySelector(".nav-row-inner-text");
+    if (step === 0) {
+        btnInnerText.innerText = `字體大小`;
+    } else if (step === 1) {
+        btnInnerText.innerText = `字體大小 (大)`;
+    } else if (step === 2) {
+        btnInnerText.innerText = `字體大小 (小)`;
+    }
+}
+
+function changeLetterSpacing() {
+    letterSpacingStep++;
+    var step = letterSpacingStep % 3;
+    if (step === 0) {
+        allElements.style.letterSpacing = 'normal';
+    } else if (step === 1) {
+        allElements.style.letterSpacing = '0.3em';
+    } else if (step === 2) {
+        allElements.style.letterSpacing = '0.5em';
+    }
+    var btnInnerText = document.querySelector(".letter-spacing-btn").querySelector(".nav-row-inner-text");
+    if (step === 0) {
+        btnInnerText.innerText = `字體間距`;
+    } else if (step === 1) {
+        btnInnerText.innerText = `字體間距 (大)`;
+    } else if (step === 2) {
+        btnInnerText.innerText = `字體間距 (更大)`;
+    }
+}
+
+// 待完成
+function changeContrast() {
+
+}
+
+function changeReset() {
+    allElements.style.fontSize = '100%';
+    allElements.style.letterSpacing = 'normal';
+    fontSizeStep = 0;
+    letterSpacingStep = 0;
+    var btnInnerText = document.querySelector(".font-size-btn").querySelector(".nav-row-inner-text");
+    btnInnerText.innerText = `字體大小`;
+    var btnInnerText = document.querySelector(".letter-spacing-btn").querySelector(".nav-row-inner-text");
+    btnInnerText.innerText = `字體間距`;
+}
+
+
+
 
 
 
@@ -54,43 +153,7 @@ var themeButton = document.getElementById("theme-button");
 //         root.classList.add("font-size-small");
 //     }
 // });
-var translatePercentage = 20;
 
-function goToLayer(layer) {
-    var mainWindow = document.querySelector('.nav-layer-1');
-    var layerWindow = document.querySelector(`.nav-layer-${layer}`);
-
-    layerWindow.classList.remove('displaynone');
-
-    setTimeout(() => {
-        mainWindow.style.transform = `translateX(-${translatePercentage}%)`;
-        mainWindow.style.opacity = '0';
-    }, 10);
-    
-    setTimeout(() => {
-        layerWindow.style.transform = 'translateX(0)';
-        layerWindow.style.opacity = '1';
-        mainWindow.classList.add('displaynone');
-    }, 100);
-}
-
-function backToFirstLayer(layer) {
-    var mainWindow = document.querySelector('.nav-layer-1');
-    var layerWindow = document.querySelector(`.nav-layer-${layer}`);
-
-    mainWindow.classList.remove('displaynone');
-
-    setTimeout(() => {
-        layerWindow.style.transform = `translateX(${translatePercentage}%)`;
-        layerWindow.style.opacity = '0';
-    }, 10);
-    
-    setTimeout(() => {
-        mainWindow.style.transform = 'translateX(0)';
-        mainWindow.style.opacity = '1';
-        layerWindow.classList.add('displaynone');
-    }, 100);
-}
 
 function handleLastMenuItemKeyDown(event) {
     if (event.key === 'Tab' && !event.shiftKey) {
